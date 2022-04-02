d71f434497ed336c314696e56a4336c4ec2fdce5
FeiFeiSeal <99242308+FeiFeiSeal@users.noreply.github.com>
2022-04-02 22:04:00 +0800

    解決頁面空白不出現的問題-1

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,7 @@
 //   transpileDependencies: true
 // })
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Portfolio-VueCLI/'
-    : '/'
+  publicPath: '/Portfolio-VueCLI/'
 }
 //https://<USERNAME>.github.io/<REPO>/
 //https://feifeiseal.github.io/Portfolio-VueCLI/
