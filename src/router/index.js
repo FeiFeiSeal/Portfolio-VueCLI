44a1027ffe710c0889e07fbcb20aeae13f19d661
FeiFeiSeal <99242308+FeiFeiSeal@users.noreply.github.com>
2022-04-02 23:36:01 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, createWebHashHistory() } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 import D1 from '../views/D1_DrumKit.vue';
 
 
