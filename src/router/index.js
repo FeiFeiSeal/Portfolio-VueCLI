2914ec8541529b402e0f32dd8dec8c7ef484d88d
FeiFeiSeal <99242308+FeiFeiSeal@users.noreply.github.com>
2022-04-02 23:26:09 +0800

    9

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, createWebHashHistory() } from 'vue-router';
 import D1 from '../views/D1_DrumKit.vue';
 
 
@@ -46,7 +46,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
