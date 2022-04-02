db500e9863d1dfb4635bcda87970ae914edc6814
FeiFeiSeal <99242308+FeiFeiSeal@users.noreply.github.com>
2022-04-02 23:30:51 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@
 // module.exports = defineConfig({
 //   transpileDependencies: true
 // })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Portfolio-VueCLI/'
-    : '/'
-}
 // module.exports = {
-//   publicPath: "./"
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/Portfolio-VueCLI/'
+//     : '/'
 // }
+module.exports = {
+  publicPath: "./"
+}
 //https://<USERNAME>.github.io/<REPO>/
 //https://feifeiseal.github.io/Portfolio-VueCLI/
